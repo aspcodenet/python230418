3984e1c49b2f3e3cf3736b58bc1c5a76b9ad5efc
aspcodenet <stefan.holmberg@systementor.se>
2023-05-04 10:02:56 +0200

    a

---
+++ testdemos/employeeTest.py
@@ -0,0 +1,12 @@
+import unittest
+from employee import Customer
+
+class CustomerTests(unittest.TestCase):
+    def test_when_not_member_and_cost_is_less_than_1000_then_price_is_unchanged(self):
+        # arrange
+       
+        price = 800 c = Customer(False,"Stefan")
+        # act
+        result = c.calculatePrice(price)
+        # assert
+        self.assertEqual(result,800)
