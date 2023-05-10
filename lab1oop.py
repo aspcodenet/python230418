71e75566edd9119b66f930481fdc58ff9e7fccb6
aspcodenet <stefan.holmberg@systementor.se>
2023-05-10 14:50:12 +0200

    save

--- lab1oop.py
+++ lab1oop.py
@@ -1,9 +1,20 @@
+import jsonpickle
 # Skapa en klass Matratt
 # Den ska ha ett namn, pris, typ, antal kalorier
 # Typ kan man tänka sig Vegetarisk, Vegansk, Kött ? ÖVERKURS ENUM?
 # Skapa upp några objekt och lägg i en lista.
 # Skriv ut en dagens lunchmeny!
 from dataclasses import dataclass
+
+def saveToFile(list):
+    f = jsonpickle.encode(list)
+    with open('food.txt', 'w') as file:
+        file.write(f)
+
+def readFromFile():
+    with open('food.txt', 'r') as file:
+        txt = file.read()
+        return jsonpickle.decode(txt)
 
 @dataclass
 class Course:
@@ -18,6 +29,8 @@ course3 = Course("Morotssoppa",90,"Vegetarisk", 50)
 lunchMeny.append(course1)
 lunchMeny.append(course2)
 lunchMeny.append(course3)
+saveToFile(lunchMeny)
+ll = readFromFile()
 
 print("*** DAGENS LUNCH ***")
 for course in lunchMeny:
